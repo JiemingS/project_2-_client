80f82913d2c3168ca999e4a7762d2fc973b1ca4a
JiemingS <sunjieming1121@gmail.com>
2019-10-22 11:43:33 -0400

    change the url

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://mysterious-sands-56739.herokuapp.com/',
+  production: 'https://mysterious-sands-56739.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
